2de955c3b3f04d90cdfabdfcb929a187046746cc
vinjer17dim <marjer17dim@gmail.com>
2017-09-30 18:43:31 +0700

    fixed something

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("key/serviceAccountKey.json");
+
+var firebase_app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-notify-24c1c.firebaseio.com"
+});
+
+console.log(firebase_app.options.credential);
+console.log(firebase_app.options.databaseURL);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
