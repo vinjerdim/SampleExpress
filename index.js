3c2d081290c419ac404276609dd774da9bd13d69
vinjer17dim <marjer17dim@gmail.com>
2017-09-30 18:19:55 +0700

    fixed something

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
@@ -9,4 +11,6 @@ app.all('/message/:userid', function(req, res) {
     res.send(req.params.userid);
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
